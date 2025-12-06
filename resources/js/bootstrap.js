e77f1bd53108eb148e452017e9b901d7358a698f
raffi10123271-cpu <raffi.10123271@mahasiswa.unikom.ac.id>
2025-12-06 15:46:45 +0700

    update fitur

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 window.axios = axios;
 import Echo from 'laravel-echo';
-import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
